d72f6bfdb8dcfd9ed45a3ed5726ddace669e2e53
Tenten <pakpichamoon@gmail.com>
2024-12-13 15:55:31 +0700

    finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
+#include<cmath>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
+
+
 int main(){	
+	double loan, rate, payamount;
 	cout << "Enter initial loan: ";
+	cin >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payamount ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +25,72 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	
+		int year = 1 ;
+		double prevb, interest, total, payment, newb ;
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+		if(payamount<loan){
+
+		prevb = loan ;
+		interest = prevb*rate/100 ;
+		payment = payamount ;
+		total = prevb + interest ;
+		newb = total - payment ;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << year; 
+			cout << setw(13) << left << prevb;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << payment;
+			cout << setw(13) << left << newb;
+			cout << "\n";
+			
+		do{
+			year = year + 1 ;
+			prevb = newb ;
+			interest = prevb*rate/100 ;
+			payment = payamount ;
+			total = prevb + interest ;
+			newb = total - payment ;
+
+			if(total<payamount){
+				payment = total ;
+				newb = 0 ;
+			}
+
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << year; 
+			cout << setw(13) << left << prevb;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << payment;
+			cout << setw(13) << left << newb;
+			cout << "\n";
+
+			
+		
+
+		}while(newb>0);
+		}else if(payamount>loan){
+			prevb = loan;
+			interest = prevb*rate/100 ;
+			total = prevb + interest ;
+			payment = total ;
+			newb = total - payment ;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << year; 
+			cout << setw(13) << left << prevb;
+			cout << setw(13) << left << interest;
+			cout << setw(13) << left << total;
+			cout << setw(13) << left << payment;
+			cout << setw(13) << left << newb;
+			cout << "\n";
+
+
+		}
+
+		
+
 	return 0;
 }
